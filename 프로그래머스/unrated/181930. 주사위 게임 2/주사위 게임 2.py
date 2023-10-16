6b3de03743c36a157dc5e896261d99463a93c8e3
PARK HO YEON <133315262+qkrtiger@users.noreply.github.com>
2023-10-16 12:46:56 +0900

    Update 주사위 게임 2.py

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
@@ -10,3 +10,13 @@ def solution(a, b, c):
         score = a + b + c
     
     return score
+
+# set 활용
+def solution(a, b, c):
+    check=len(set([a,b,c]))
+    if check==1:
+        return 3*a*3*(a**2)*3*(a**3)
+    elif check==2:
+        return (a+b+c)*(a**2+b**2+c**2)
+    else:
+        return (a+b+c)
