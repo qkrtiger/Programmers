a23e8ce5c7f4b18ffc1204ec04920011a6e9e5a3
PARK HO YEON <133315262+qkrtiger@users.noreply.github.com>
2023-10-17 12:56:11 +0900

    Update 폰켓몬.py

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -2,3 +2,7 @@ def solution(nums):
     unique_pokemon = set(nums)  # 중복을 제외한 폰켓몬 종류를 구함
     max_pokemon = len(nums) // 2  # 선택할 수 있는 최대 폰켓몬 수
     return min(max_pokemon, len(unique_pokemon))
+
+# 간결 코드
+def solution(ls):
+    return min(len(ls)/2, len(set(ls)))
