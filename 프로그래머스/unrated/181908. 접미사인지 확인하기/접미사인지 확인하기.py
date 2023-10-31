0995987afccefa83d34420a76ba995d918ff591f
PARK HO YEON <133315262+qkrtiger@users.noreply.github.com>
2023-10-31 23:27:28 +0900

    [level unrated] Title: 접미사인지 확인하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -0,0 +1,8 @@
+def solution(my_string, is_suffix):
+    suffixes = [my_string[i:] for i in range(len(my_string))]
+    
+    # is_suffix와 일치하는 접미사를 찾아 1을 반환
+    if is_suffix in suffixes:
+        return 1
+    else:
+        return 0
